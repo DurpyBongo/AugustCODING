0b47edaca2c488ab7dd0685dc65d5b78c7464baa
Ethan <ethanyichenzhu@gmail.com>
2026-08-28 09:47:34 -0400

    come back later

---
+++ CCC '01 S2 - Spirals.py
@@ -0,0 +1,26 @@
+start = int(input())
+endd = int(input())
+number_of_numbers = endd - start + 1
+directions_horizontal = [1, -1]
+directions_vertical = [1, -1]
+grid = [[" "] * 100 for _ in range(100)]
+
+row = 50
+col = 50
+
+current_num = start
+steps_to_take = 1
+
+grid[row][col] = str(current_num)
+
+while current_num < endd:
+    for direction in range(2):
+        for _ in range(steps_to_take):
+            if current_num >= endd:
+                break
+            row += directions_vertical[direction]
+            col += directions_horizontal[direction]
+            current_num += 1
+            grid[row][col] = str(current_num)
+    steps_to_take += 1
+print
